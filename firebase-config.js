cecc6777b619f1a561d1c11ec7b4fbe77c008f1d
dilipbathena <106613109+dilipbathena@users.noreply.github.com>
2026-08-12 11:18:04 +0530

    Update Firebase imports to use CDN version

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,6 @@
 // Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use

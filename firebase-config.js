ea5edd1055541fed99ea6eda36e1a8b64caaa006
dilipbathena <106613109+dilipbathena@users.noreply.github.com>
2026-08-12 11:34:28 +0530

    Update Firebase configuration with auth and analytics
    Added Google Auth provider and analytics initialization.

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,8 @@
-// Import the functions you need from the SDKs you need
+// Import the functions you need from the SDKs
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-analytics.js";
+
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -19,3 +19,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
+
+// Export the auth instances for use in index.html
+export { auth, googleProvider, signInWithPopup, signOut };
